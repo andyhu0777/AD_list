6f281fdf64fdb57ca0d35d7d4c6c1a69fbc26238
hu.andy/胡安迪_沪_开发 <andy.hu@51job.com>
2016-03-29 16:18:33 +0800

    (untested) added ajax master

--- ad_list.js
+++ ad_list.js
@@ -15,11 +15,18 @@ $(document).ready(function() {
     var page_selector = new PageSelector(options.page_size, options.max_item);
     console.log(page_selector);
 
+    // initialize ajax master
+    var master = new AjaxMaster('', page_selector);
+    master.refresh();
 
     // set page select events
     prev_page_trigger.click(function() {
+        page_selector.prev_page(); 
+        master.refresh(); 
     });
     next_page_trigger.click(function() {
+        page_selector.next_page();
+        master.refresh();
     });
 });
 
@@ -36,6 +43,37 @@ function parse_options(list_container)
 }
 
 
+class AjaxMaster {
+    construct(server_url, selector)
+    {
+        this.server_url = server_url;
+        this.selector = selector;
+    }
+
+    refresh()
+    {
+        $.ajax({
+            url: this.server_url,
+            method: "POST",
+            data: {
+               content: "",
+               page_size: selector.get_page_size(),
+               page_num: selector.get_page_num(),
+            },
+            success(on_success)
+        });
+    }
+    on_success(data)
+    {
+       json = JSON.parse(data);
+       this.selector.set_max_item(json.max_item);
+       console.log(this.selector);
+       update_content();
+    }
+    update_content()
+    {
+    }
+}
 
 class PageSelector {
     constructor(page_size, max_item)
@@ -43,12 +81,23 @@ class PageSelector {
         this.page_size = page_size;
         this.max_item = max_item;
         this.page_num = 1;
-        this.max_page_num = Math.floor((this.max_item + this.page_size - 1) / this.page_size);
+    }
+
+    get_max_page_num()
+    {
+        return Math.floor((this.max_item + this.page_size - 1) / this.page_size);
+    }
+    set_max_item(max_item) {
+        this.max_item = max_item;
     }
 
     set_page_size(page_size)
     {
         this.page_size = page_size;
+    }
+    get_page_size(page_size)
+    {
+        return this.page_size
     }
 
     set_page_num(page_num) 
