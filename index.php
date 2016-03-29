4c5613ec4e43dec9aa4414f2ea2204fe2cfb6d98
hu.andy/胡安迪_沪_开发 <andy.hu@51job.com>
2016-03-29 14:32:17 +0800

    add stylesheet to index.php, and make index.php a typical table

--- index.php
+++ index.php
@@ -5,13 +5,72 @@
 
     <head>
         <title> Test for list </title> 
+        <link rel="stylesheet" type="text/css" href="index.css" >
     </head>
 
     <body>
-        <div class="ad_list" ad_checkbox = "true" ad_del = "true">
+        <div class="r_table ad_list" ad_checkbox = "true" ad_del = "true">
+            <div class ="r_table_row">
+                <div class="r_table_heading"> Name </div>
+                <div class="r_table_heading"> Age </div>
+                <div class="r_table_heading"> Sex </div>
+           </div>
+           <div class="r_table_row"> 
+                <div class="r_table_cell">
+                    John
+                </div>
+                <div class="r_table_cell">
+                    22
+                </div>
+                <div class="r_table_cell">
+                    Male
+                </div>
+           </div> 
+           <div class="r_table_row"> 
+                <div class="r_table_cell">
+                    Anderson
+                </div>
+                <div class="r_table_cell">
+                    35
+                </div>
+                <div class="r_table_cell">
+                    Male
+                </div>
+           </div> 
+           <div class="r_table_row"> 
+                <div class = "r_table_cell">
+                    Marry
+                </div>
+                <div class="r_table_cell">
+                    35
+                </div>
+                <div class="r_table_cell">
+                    Female
+                </div>
+           </div> 
+           <div class="r_table_row"> 
+                <div class="r_table_cell">
+                    Jack
+                </div>
+                <div class="r_table_cell">
+                    27
+                </div>
+                <div class="r_table_cell">
+                    Male
+                </div>
+           </div> 
+           <div class="r_table_row"> 
+                <div class="r_table_cell">
+                    Quin
+                </div>
+                <div class="r_table_cell">
+                    32
+                </div>
+                <div class="r_table_cell">
+                    Male
+                </div>
+           </div> 
         </div>
-
-
 
 
         <script src="jquery-2.2.2.min.js"> </script>
